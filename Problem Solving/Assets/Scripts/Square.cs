97ab8fed92435a93ce62c4e17ef86545942905c0
FahriDarmawan <44422245+FahriDarmawan@users.noreply.github.com>
2021-10-10 01:24:19 +0700

    Problem 07

---
+++ Problem Solving/Assets/Scripts/Square.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ScoreManager.Instance.IncreaseScore(1);
+        Destroy(gameObject);
+    }
+}
