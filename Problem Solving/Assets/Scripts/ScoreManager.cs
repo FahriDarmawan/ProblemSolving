ce3adc00318f49699753e2284aca090905042d3c
FahriDarmawan <44422245+FahriDarmawan@users.noreply.github.com>
2021-10-10 02:13:26 +0700

    Problem 08

---
+++ Problem Solving/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static ScoreManager _instance = null;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<ScoreManager>();
+                if(_instance == null)
+                {
+                    Debug.LogError("Fatal Error : ScoreManager not found");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    int score = 0;
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetCurrentScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetCurrentScore()
+    {
+        score = 0;
+    }
+
+    public void IncreaseScore(int scoreCount)
+    {
+        score += scoreCount;
+        scoreText.text = score.ToString();
+    }
+}
---
+++ Problem Solving/Assets/Scripts/SquareSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareSpawner : MonoBehaviour
+{
+    #region Singleton
+
+    private static SquareSpawner _instance = null;
+
+    public static SquareSpawner Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<SquareSpawner>();
+                if(_instance == null)
+                {
+                    Debug.LogError("Fatal Error : SquareSpawner not Found");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public GameObject squarePrefabs;
+
+    int maxSquare = 10;
+
+    float randomX;
+    float randomY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i <= Random.Range(1, maxSquare); i++)
+        {
+            SpawnSquare();
+        }
+    }
+
+    public void SpawnSquare()
+    {
+        randomX = Random.Range(-8, 8);
+        randomY = Random.Range(-4, 4);
+        GameObject newSquare = Instantiate(squarePrefabs);
+        newSquare.transform.position = new Vector2(randomX, randomY);
+    }
+}
